a238c68f89d1095c9d093b3a3fa96f9cbd1635de
shu0803 <100079864@students.swinburne.edu.my>
2019-10-02 00:40:49 +0800

    fixed some bugs for the dashboard and chart page front-end

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,47 @@
-$(document).ready(function(){
-		var date_input=$('input[name="startDate"]'); //our date input has the name "startDate"
-		date_input.datepicker({
-			format: 'dd/mm/yyyy',
-			todayHighlight: true,
-			autoclose: true,
-		})
-        
-    var date_input=$('input[name="endDate"]'); //our date input has the name "endDate"
-		date_input.datepicker({
-			format: 'dd/mm/yyyy',
-			todayHighlight: true,
-			autoclose: true,
-		})
-		
-		var date_input=$('input[name="date"]'); //our date input has the name "date"
-		date_input.datepicker({
-			format: 'dd/mm/yyyy',
-			todayHighlight: true,
-			autoclose: true,
-		})
-	})
+//$(document).ready(function(){
+//		var date_input=$('input[name="startDate"]'); //our date input has the name "startDate"
+//		date_input.datepicker({
+//			format: 'dd/mm/yyyy',
+//			todayHighlight: true,
+//			autoclose: true,
+//		})
+//        
+//    var date_input=$('input[name="endDate"]'); //our date input has the name "endDate"
+//		date_input.datepicker({
+//			format: 'dd/mm/yyyy',
+//			todayHighlight: true,
+//			autoclose: true,
+//		})
+//		
+//		var date_input=$('input[name="date"]'); //our date input has the name "date"
+//		date_input.datepicker({
+//			format: 'dd/mm/yyyy',
+//			todayHighlight: true,
+//			autoclose: true,
+//		})
+//});
 
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-});
+//$(document).ready(function() {
+//		$('.tooltip').tooltipster();
+//});
+//
+//$('.tooltip').tooltipster({
+//    theme: 'tooltipster-noir'
+//});
+
+function showDashboard(){
+var xhttp = new XMLHttpRequest();
+xhttp.responseType = 'json';
+
+xhttp.onreadystatechange = function () {
+	if(this.readyState == 4 && this.status == 200) {
+		var result = this.response;
+		console.log(result);
+//		document.getElementById("test").innerHTML = ;
+	}
+};
+
+xhttp.open("GET","http://localhost:3000/api/room",true);
+xhttp.send();
+	
+};
