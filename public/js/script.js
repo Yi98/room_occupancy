d308e1f478979ddbd7ea8bf148fd4211282e4503
shu0803 <100079864@students.swinburne.edu.my>
2019-10-17 18:45:51 +0800

    added search room function to the dashboard

--- public/js/script.js
+++ public/js/script.js
@@ -33,6 +33,23 @@
 //$(document).ready(function(){
 //  $('[data-toggle="tooltip"]').tooltip();
 //});
+function searchRoom(){
+	var input, filter, ul, li, i, a, txtValue;
+	input = document.getElementById("search");
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("showRoom");
+	li = ul.getElementsByTagName("div");
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("h4")[0];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+				li[i].style.display = "";
+		} else {
+				li[i].style.display = "none";
+		}
+	}
+}
+
 
 function on() {
   document.getElementById("overlay").style.display = "block";
