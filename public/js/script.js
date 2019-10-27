88521d4f96739142a02665066c6e6fa645f16b97
Florence9827 <florencesnteo@hotmail.com>
2019-10-27 22:46:44 +0800

    add serach function for user table

--- public/js/script.js
+++ public/js/script.js
@@ -642,6 +642,27 @@ function showUserTable(){
 	
 };
 
+function search() {
+    var input, filter, table, tr, td, i, textValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("userTable");
+    tr = table.getElementsByTagName("tr");
+    
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            textValue = td.textContent || td.innerText;
+            if (textValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+            
+        }
+    }
+};
+
 function addUser() {
     
     if(document.getElementById("role").value == "Pick a Role")
