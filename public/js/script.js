fc22f568f6b80e977c2066648a7ffdba2da1f01a
Yi98 <ngyi07285@hotmail.com>
2019-10-29 23:00:48 +0800

    allow mouse wheel scrolling through room cards

--- public/js/script.js
+++ public/js/script.js
@@ -1836,3 +1836,28 @@ function onRoomClicked(roomName, roomId) {
 	xhttp.send();
 
 }
+
+
+function horizontalWheelScroll() {
+	function scrollHorizontally(e) {
+			e = window.event || e;
+			var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+			document.getElementById('scrolling-wrapper').scrollLeft -= (delta*40); // Multiplied by 40
+			e.preventDefault();
+	}
+	if (document.getElementById('scrolling-wrapper').addEventListener) {
+			// IE9, Chrome, Safari, Opera
+			document.getElementById('scrolling-wrapper').addEventListener("mousewheel", scrollHorizontally, false);
+			// Firefox
+			document.getElementById('scrolling-wrapper').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+	} else {
+			// IE 6/7/8
+			document.getElementById('scrolling-wrapper').attachEvent("onmousewheel", scrollHorizontally);
+	}
+}
+
+function onLoadDashboard() {
+	showDashboardRooms();
+	loadTodayTrend();
+	horizontalWheelScroll();
+}
