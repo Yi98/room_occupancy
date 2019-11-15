cd9321698220dd1af9ea1f47790eefbdf1fb05ee
Yi98 <ngyi07285@hotmail.com>
2019-11-15 19:18:05 +0800

    removed auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    let token;
-    let isToken = false;
+    // let token;
+    // let isToken = false;
 
     // if (req.headers.cookie) {
     //   tokens = req.headers.cookie.split(';');
@@ -34,24 +34,26 @@ module.exports = (req, res, next) => {
     // }
 
 
-    if (req.headers.cookie) {
-      tokens = req.headers.cookie.split(';');
-      for (let i=0; i<tokens.length; i++) {
-        info = tokens[i].split("=");
-        for (let j=0; j<info.length; j++) {
-          if (info[j].trim() == "token") {
-            token = (info[j+1]).trim();
-            const decoded = jwt.verify(token, 'fyp_room');
-            req.userData = { userId: decoded.userId, role: decoded.role };
-            console.log(token);
-            next();
-          }
-        }
-      }
-    }
-    else {
-      throw new Error();
-    }
+    // if (req.headers.cookie) {
+    //   tokens = req.headers.cookie.split(';');
+    //   for (let i=0; i<tokens.length; i++) {
+    //     info = tokens[i].split("=");
+    //     for (let j=0; j<info.length; j++) {
+    //       if (info[j].trim() == "token") {
+    //         token = (info[j+1]).trim();
+    //         const decoded = jwt.verify(token, 'fyp_room');
+    //         req.userData = { userId: decoded.userId, role: decoded.role };
+    //         console.log(token);
+    //         next();
+    //       }
+    //     }
+    //   }
+    // }
+    // else {
+    //   throw new Error();
+    // }
+
+    next();
     
 
   }
